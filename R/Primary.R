ebf139303dc4ae8da93630cedf4bfc212461b30a
Langsrud <oyl@ssb.no>
2022-03-04 18:16:02 +0100

    Parameter eachPrimary to capture output from each primary function

--- R/Primary.R
+++ R/Primary.R
@@ -1,5 +1,5 @@
 # combination of primary functions 
-Primary <- function(primary, crossTable, ...) {
+Primary <- function(primary, crossTable, eachPrimary = FALSE, ...) {
   num <- NULL
   pri <- 1L
   n <- nrow(crossTable)    # This line is why crossTable is parameter
@@ -23,6 +23,18 @@ Primary <- function(primary, crossTable, ...) {
     }
     if (length(a) != n)
       stop("wrong length of primary function output")
+    if (eachPrimary) {
+      if (is.numeric(eachPrimary)) {
+        pnum <- as.data.frame(matrix(as.integer(a), dimnames = list(NULL, paste0("primary", i))))
+      } else {
+        pnum <- as.data.frame(matrix(            a, dimnames = list(NULL, paste0("primary", i))))
+      }
+      if (is.null(num)) {
+        num <- pnum
+      } else {
+        num <- cbind(num, pnum)
+      }
+    }
     pri <- pri * as.integer(!a)    # zeros (=TRUE since !) and NA’s are preserved
   }
   pri <- !as.logical(pri)
