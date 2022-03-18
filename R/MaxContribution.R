80ca17675ff52dce3c1a5dc92302eafc0bfe7114
Langsrud <oyl@ssb.no>
2022-03-18 17:45:30 +0100

    MaxContribution extended with groups as parameter Seems to work

--- R/MaxContribution.R
+++ R/MaxContribution.R
@@ -9,6 +9,9 @@
 #' @param n Number of contributors to be found
 #' @param decreasing Ordering parameter. Smallest contributors found when \code{FALSE}. 
 #' @param index Indices to \code{y} returned when TRUE
+#' @param groups When non-NULL, major contributions after aggregation within groups.
+#'               Cannot be combined with `index = TRUE`. 
+#'               The missing group category is excluded.
 #'
 #' @return Matrix with lagest contributors in first column, second largest in second column and so on.  
 #' @export
@@ -35,9 +38,26 @@
 #' 
 #' b <- ModelMatrix(z[, -4], crossTable = TRUE, inputInOutput = c(TRUE, FALSE, TRUE))
 #' 
-#' cbind(b$crossTable, MaxContribution(b$modelMatrix, z$ths_per, 10))
+#' k <- cbind(b$crossTable, MaxContribution(b$modelMatrix, z$ths_per, 10))
 #' 
-MaxContribution <- function(x, y, n = 1, decreasing = TRUE, index = FALSE) {
+#' gr18 <- paste0("g", 1:18)                          # Each row is a group
+#' k18 <- cbind(b$crossTable, MaxContribution(b$modelMatrix, z$ths_per, 10, groups = gr18))
+#' identical(k, k18) # TRUE
+#' 
+#' gr9 <- paste0("g", as.integer(10 * z$ths_per)%%10) # 9 groups from decimal
+#' k9 <- cbind(b$crossTable, MaxContribution(b$modelMatrix, z$ths_per, 10, groups = gr9))
+#' 
+#' k18[c(4, 13, 17, 33), ]
+#' k9[c(4, 13, 17, 33), ]
+#' 
+MaxContribution <- function(x, y, n = 1, decreasing = TRUE, index = FALSE, groups = NULL) {
+  
+  if (!is.null(groups)) {
+    if (index) {
+      stop("index when groups is not implemented")
+    }
+    return(MaxContributionGroups(x = x, y = y, n = n, decreasing = decreasing, groups = groups))
+  }
   
   ordy <- order(y, decreasing = decreasing)
   
@@ -73,3 +93,48 @@ MaxContribution <- function(x, y, n = 1, decreasing = TRUE, index = FALSE) {
   
   maxC
 }
+
+
+
+MaxContributionGroups <- function(x, y, n = 1, decreasing = TRUE, groups) {
+  
+  if (length(groups) != nrow(x)) {
+    stop("Incorrect length of groups")
+  }
+  
+  if (anyNA(groups)) {
+    rows <- !is.na(groups)
+    groups <- groups[rows]
+    x <- x[rows, , drop = FALSE]
+  }
+  
+  groups <- as.integer(factor(groups))
+  
+  xT <- as(drop0(x), "dgTMatrix")
+  
+  xM <- data.frame(y = -decreasing * y[xT@i + 1], col = xT@j + 1, gr = groups[xT@i + 1])  # row = xT@i + 1 
+  
+  xM <- aggregate(xM["y"], xM[c("col", "gr")], sum)[c("y", "col", "gr")]
+  
+  xM <- as.matrix(xM)  # Needed since empty index below 
+  
+  xM <- SortRows(xM)  # better to rewrite to order with decreasing parameter 
+  
+  xM[, "y"] <- -decreasing * xM[, "y"]
+  
+  seqCol <- seq_len(ncol(x))
+  
+  maxC <- matrix(NA_integer_, ncol(x), n)
+  
+  for (i in seq_len(n)) {
+    if (i > 1) {
+      xM[ma, "col"] <- 0
+    }
+    ma <- match(seqCol, xM[, "col"])
+    maxC[, i] <- xM[ma, "y"]
+  }
+  
+  maxC
+}
+
+
