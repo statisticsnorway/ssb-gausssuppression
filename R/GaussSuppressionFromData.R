b88a66b9e60a88ba0938c62176b8cc0da8a6906a
Langsrud <oyl@ssb.no>
2022-06-03 11:34:26 +0200

    Move SingletonDefault to file Singleton

--- R/GaussSuppressionFromData.R
+++ R/GaussSuppressionFromData.R
@@ -489,37 +489,6 @@ GaussSuppressionFromData = function(data, dimVar = NULL, freqVar=NULL, numVar = 
 
 
 
-  
-
-#' SingletonDefault
-#'
-#' Function for \code{\link{GaussSuppressionFromData}}
-#' 
-#' This function marks input cells as singletons according to the input frequencies. 
-#' Zeros are set to singletons when `protectZeros` or `secondaryZeros` is `TRUE`. 
-#' Otherwise, ones are set to singletons.
-#'
-#' @param data  Input data, possibly pre-aggregated within `GaussSuppressionFromData`  
-#' @param freqVar A single variable holding counts (input to `GaussSuppressionFromData`)
-#' @param protectZeros Suppression parameter (see `GaussSuppressionFromData`)
-#' @param secondaryZeros Suppression parameter (see `GaussSuppressionFromData`)
-#' @param ... Unused parameters 
-#'
-#' @return singleton, \code{\link{GaussSuppression}} input 
-#' @export
-#' @keywords internal
-SingletonDefault <- function(data, freqVar, protectZeros, secondaryZeros, ...) {
-  
-  if(is.null(protectZeros))   stop("A non-NULL value of protectZeros is required.")
-  if(is.null(secondaryZeros)) stop("A non-NULL value of secondaryZeros is required.")
-  
-  if (protectZeros | secondaryZeros){ 
-    return(data[[freqVar]] == 0)
-  }
-  data[[freqVar]] == 1
-}
-
-
 
 
 
