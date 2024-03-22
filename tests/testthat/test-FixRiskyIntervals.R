2d9fa380c1ce65ff4a9137754b7180665acd850e
Langsrud <oyl@ssb.no>
2024-03-22 14:51:57 +0100

    Simplify test and change from Rsymphony to Rglpk in the test.

--- tests/testthat/test-FixRiskyIntervals.R
+++ tests/testthat/test-FixRiskyIntervals.R
@@ -1,10 +1,7 @@
-
-#for( lpPackage in c("lpSolve", "Rsymphony", "Rglpk", "highs"))
-  
-  
-for( lpPackage in "Rsymphony")  
-if (require(lpPackage, character.only = TRUE, quietly = TRUE)) {
-  test_that("FixRiskyIntervals", {
+test_that("FixRiskyIntervals", {
+  lpPackage <- "Rglpk"
+  skip_if_not_installed(lpPackage)
+  if (require(lpPackage, character.only = TRUE, quietly = TRUE)) {
     z3 <- SSBtoolsData("z3")
     upper <- z3$region %in% LETTERS
     z3$region[upper] <- paste0(z3$region[upper], 2)
@@ -15,11 +12,11 @@ if (require(lpPackage, character.only = TRUE, quietly = TRUE)) {
     s <- sample.int(nrow(z3), size = 400)
     f <- ~(region + fylke) * mnd2 + kostragr * hovedint * mnd
     co <- capture.output({
-    b <- SuppressDominantCells(z3[s, ], numVar = "value", n = 1:2, k = c(70, 95), 
-                               formula = f, lpPackage =  lpPackage , 
-                               rangePercent = 200,  
-                               rangeMin = 1)
+      b <- SuppressDominantCells(z3[s, ], numVar = "value", n = 1:2, k = c(70, 95), 
+                                 formula = f, lpPackage =  lpPackage , 
+                                 rangePercent = 50,  
+                                 rangeMin = 1)
     })
-    expect_equal(as.vector(table(b$suppressed_integer)), c(130L, 39L, 33L, 37L))
-  })
-} 
+    expect_equal(as.vector(table(b$suppressed_integer)), c(164L, 39L, 33L, 3L))
+  }
+})
